00216116a5a7b0fddc7446f52c981278269fc2d5
JCWoodworker <jc@rilocalwoodworks.com>
2023-10-06 19:58:42 -0400

    updated dropdown styling

--- src/pages/compare_teammates/CompareTeammates.tsx
+++ src/pages/compare_teammates/CompareTeammates.tsx
@@ -1,24 +1,53 @@
 import React, { useState } from "react"
 import Select from "react-select"
 export const CompareTeammates: React.FC = () => {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const [myTeam, setMyTeam] = useState(() =>
 		JSON.parse(window.localStorage.getItem("myFantasyTeam"))
 	)
+
+	const styles = {
+		container: (styles) => ({
+			...styles,
+			width: "20rem",      
+		}),
+    control: (styles) => ({
+      ...styles,
+      backgroundColor: "black",
+      color: "white",
+    }),
+    option: (styles, { isFocused }) => ({
+      ...styles,
+      backgroundColor: isFocused ? "white" : "black",
+      color: isFocused ? "black" : "white",
+    }),
+    menu: (styles) => ({
+      ...styles,
+      backgroundColor: "black",
+      color: "white",
+    }),
+	}
 	return (
 		<>
 			<h2>Compare Teammates</h2>
 			<div className="teammate-comparison-selection-container">
 				<div>
-        <Select options={myTeam.map((player) => ({
-        value: player.PlayerID,
-        label: player.FullName
-      }))} />
+					<Select
+						options={myTeam.map((player) => ({
+							value: player.PlayerID,
+							label: player.FullName,
+						}))}
+            styles={styles}
+					/>
 				</div>
 				<div>
-        <Select options={myTeam.map((player) => ({
-        value: player.PlayerID,
-        label: player.FullName
-      }))} />
+					<Select
+						options={myTeam.map((player) => ({
+							value: player.PlayerID,
+							label: player.FullName,
+						}))}
+						styles={styles}
+					/>
 				</div>
 			</div>
 		</>
